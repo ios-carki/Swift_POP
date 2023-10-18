28017a2916f8378dac84a1b8a7de44efe101430f
carki <94.nogari@gmail.com>
2023-10-18 23:35:03 +0900

    Initial Commit

---
+++ Swift_POP/Swift_POPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_POPApp.swift
+//  Swift_POP
+//
+//  Created by Carki on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct Swift_POPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
